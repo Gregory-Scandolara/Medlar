625dd1c625e207b440ae782932abbe837a6a879e
gregory1302 <40505479+gregory1302@users.noreply.github.com>
2022-10-17 20:30:41 -0300

    Add files via upload

---
+++ telastcc/deleta_remed.php
@@ -0,0 +1,14 @@
+<?php
+include('conexao.php');
+$idremedio = $_GET['idremedio'];
+
+$sql = "DELETE FROM medicamentos WHERE idremedio=$idremedio";
+//descobrir forma de excluir os remedios do idoso junto 
+mysqli_query($con, $sql);
+if (mysqli_affected_rows($con) > 0) {
+    header("Location: dado_remed.php");
+} else {
+    echo "<script>alert('Houve algum erro.');</script>";
+    mysqli_error($con);
+    echo $con->error;
+}
--- telastcc/editar_estoque.php
+++ telastcc/editar_estoque.php
@@ -4,78 +4,67 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/cadastro.css">
     <title>Document</title>
 </head>
 
 <body>
     <?php
     include('conexao.php');
-    include('navbar.html');
 
-    $ididoso = $_GET['ididoso'];
+    $idremedio = $_GET['idremedio'];
 
     if (isset($_POST['editar'])) {
         $idremedio = $_POST['idremedio'];
         $quant_caixa = $_POST['quant_caixa'];;
-        $unid_caixa = $_POST['unid_caixa'];
+        $add_cp = $_POST['add_cp'];
 
-        $sql = "UPDATE idosos SET 
-                nome_idoso='$nome_idoso', 
-                nascimento='$nascimento', 
-                genero='$genero',
-                alergia='$alergia' ,
-				comorbidade='$comorbidade' ,
-				obs_estoque='$obs_estoque' ,
-				cpf='$cpf',
-				nome_resp='$nomeresp' ,
-				telefone_resp='$telefoneresp' ,
-				cpf_resp='$cpf_resp' ,
-				parentesco='$parentesco' ,
-				endereco_resp='$enderecoresp' 
-            WHERE ididoso='$ididoso'";
+        $sql = "DELETE FROM estoque WHERE idremedio=$idremedio";
 
         mysqli_query($con, $sql);
-        header('Location: dadoidoso.php?ididoso=' . $ididoso);
-        echo $sql;
+
+        $sql2 = "insert into estoque (idremedio, caixas ,unid_cp, add_cp)
+                values ('$idremedio', '$quant_caixa', '0', '$add_cp')";
+
+        mysqli_query($con, $sql2);
+
+        header('Location: estoque.php');
+        echo $sql2;
         // if (mysqli_affected_rows($con) > 0) {
         //     echo "<script> alert('Usuário alterado com sucesso.') </script>";
         //     // header("Location: listaUsuarios.php");
         // } else {
         //     echo "<script> alert('Ocorreu algum erro.') </script>";
         // }
-    }
-    $sql = "SELECT * FROM idosos WHERE ididoso=$ididoso";
-    $rs = mysqli_query($con, $sql);
-    $linha = mysqli_fetch_array($rs);
+    } else {
+        $sql = "SELECT SUM(caixas) as caixas, SUM(add_cp) as add_cp FROM estoque WHERE idremedio=$idremedio";
+        $rs = mysqli_query($con, $sql);
+        $linha = mysqli_fetch_array($rs);
     ?>
-    <?php include('navbar.html'); ?>
-    <?php
+        <?php include('navbar.html'); ?>
+        <?php
 
 
-    include("conexao.php");
-    ?>
-    <title>Cadastro Idosos</title>
-    <div class='container' id="cad-conteiner3">
-        <form action="cadastro_estoque.php" method="POST">
-            <br><br>
-            <label>Medicamento <select class='form-control' name="idremedio"></label>
-            <option>Selecione</option>
-            <?php $sql = "select * from medicamentos ";
-            $rs = mysqli_query($con, $sql);
-            while ($linha = mysqli_fetch_array($rs)) { ?>
-                <option value=<?php echo $linha['idremedio'] ?>>
-                    <?php echo $linha['nome_remed'] . ' - ' . $linha['dosagem'] . 'mg'; ?>
-                </option>
-            <?php } ?> </select>
+        include("conexao.php");
+        ?>
+        <title>Cadastro Idosos</title>
+        <div class='container' id="cad-estoque-conteiner">
+            <form action="editar_estoque.php?idremedio=<?php echo $idremedio ?>" method="POST">
+                <br><br>
+                <label>Medicamento </label>
+                <?php $sql2 = "select * from medicamentos where idremedio = $idremedio ";
+                $rs2 = mysqli_query($con, $sql2);
+                $linha2 = mysqli_fetch_array($rs2) ?>
+                <?php echo $linha2['nome_remed'] . ' - ' . $linha2['dosagem'] . 'mg'; ?>
+                <label>Quantidade de caixas</label> <br><input value="<?php echo $linha['caixas']; ?>" class='form-control' type="text" name="quant_caixa">
+                <label>Quantidade total de comprimidos</label> <br><input value="<?php echo $linha['add_cp']; ?>" class='form-control' type="text" name="add_cp"> <br>
+                <input type="hidden" name="idremedio" value="<?php echo $idremedio ?>">
+                <input class='btn btn-success' type="submit" value="Enviar" name="editar" />
+                <input class='btn btn-info' type="reset" value="Limpar campos" />
+        </div>
 
-            <label>Quantidade de caixas</label> <br><input class='form-control' type="text" name="quant_caixa">
-            <label>Quantidade de remedios em cada caixa</label> <br><input class='form-control' type="text" name="unid_caixa"> <br>
-
-            <input class='btn btn-success' type="submit" value="Enviar" name="btnSalvar" />
-            <input class='btn btn-info' type="reset" value="Limpar campos" />
-    </div>
-
-    </div>
+        </div>
+    <?php } ?>
 </body>
 
 </html>
